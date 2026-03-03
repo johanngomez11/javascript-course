eca5ccaa26a00fa2231dfe616c6b47ecf3fd7585
johanngomez11 <johann272521@gmail.com>
2026-03-02 19:14:07 -0600

    Ejercicios de condicionales terminados

--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -44,13 +44,56 @@ if (age >= 18) {
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
-const isAdult = age == 17 ? "Es mayor de edad" : "No es mayor de edad"
+const isAdult = age >= 18 ? "Es mayor de edad" : "No es mayor de edad"
 console.log(isAdult)
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
 let month = 8
 let season
 
+if (month >= 3 && month <= 5){
+    season = "Primavera"
+} else if (month >= 6 && month <= 8){
+    season = "Verano"
+} else if (month >= 9 && month <= 11){
+    season = "Otoño"
+} else if (month === 12 || month <= 2){
+    season = "Invierno"
+} else {
+    season = "Número de mes incorrecto"
+}
+
+console.log(season)
+
+// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
+// switch
+if (month = 1, 3, 5, 7, 8, 10, 12) {
+    console.log("El mes tiene 31 días")
+} else if (month = 4, 6, 9, 11) {
+    console.log("El mes tiene 30 días")
+} else if (month = 2) {
+    console.log("El mes tiene 28 o 29 días")
+} else {
+    console.log("Número de mes incorrecto")    
+}
+
+// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+let languaje = "japon"
+switch (languaje) {
+    case "español":
+        console.log("Hola")
+        break;
+    case "ingles":
+        console.log("Hello")
+        break;
+    case "japon":
+        console.log("Konishiwa")
+        break;
+    default:
+        break;
+}
+// 9. Usa un switch para hacer de nuevo el ejercicio 6
+month = 11
 switch (month) {
     case 3:
     case 4: 
@@ -77,26 +120,28 @@ switch (month) {
 }
 console.log(season)
 
-// Otra manera
-if (month >= 3 && month <= 5){
-    season = "Primavera"
-} else if (month >= 6 && month <= 8){
-    season = "Verano"
-} else if (month >= 9 && month <= 11){
-    season = "Otoño"
-} else if (month === 12 || month <= 2){
-    season = "Invierno"
-} else {
-    season = "Número de mes incorrecto"
-}
-
-console.log(season)
-
-// 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
-// switch
-
-// 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
-
-// 9. Usa un switch para hacer de nuevo el ejercicio 6
-
 // 10. Usa un switch para hacer de nuevo el ejercicio 7
+let numberOfDays
+switch (month) {
+    case 1:
+    case 3: 
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        numberOfDays = "El mes tiene 31 días"
+        break
+    case 4: 
+    case 6:
+    case 9:
+    case 11:
+        numberOfDays = "El mes tiene 30 días"
+        break
+    case 2:
+        numberOfDays = "El mes tiene 28 o 29 días"
+        break
+    default:
+        numberOfDays = "Número de mes incorrecto"
+}
+console.log(numberOfDays)
